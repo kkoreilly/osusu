293bddc73f3a6e48d31e4459f2111a0ce8fdc39e
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-15 10:50:07 -0800

    started db

---
+++ osusu/db.go
@@ -0,0 +1,19 @@
+package osusu
+
+import (
+	"goki.dev/rqlite"
+	"gorm.io/gorm"
+)
+
+// DB is the gorm database for the app.
+var DB *gorm.DB
+
+// OpenDB opens and sets up the database.
+func OpenDB() error {
+	db, err := gorm.Open(rqlite.Open("http://"))
+	if err != nil {
+		return err
+	}
+	DB = db
+	return db.AutoMigrate()
+}
