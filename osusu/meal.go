c2cfe5ad589a7d6b981f9cf5bf3926ee31804008
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-17 11:51:29 -0800

    added categories and cuisines

--- osusu/meal.go
+++ osusu/meal.go
@@ -2,10 +2,51 @@ package osusu
 
 import "gorm.io/gorm"
 
+//go:generate enumgen -sql
+
 type Meal struct {
 	gorm.Model  `view:"-"`
 	Name        string
 	Description string
 	Source      string
 	Image       string
+	Category    Categories
+	Cuisine     Cuisines
 }
+
+type Categories int64 //enums:bitflag
+
+const (
+	Breakfast Categories = iota
+	Brunch
+	Lunch
+	Dinner
+	Dessert
+	Snack
+	Appetizer
+	Side
+	Drink
+	Ingredient
+)
+
+type Cuisines int64 //enums:bitflag
+
+const (
+	African Cuisines = iota
+	American
+	Asian
+	British
+	Chinese
+	European
+	French
+	Greek
+	Indian
+	Italian
+	Japanese
+	Jewish
+	Korean
+	LatinAmerican
+	Mexican
+	MiddleEastern
+	Thai
+)
